19f5dfd8eb7522f1379d2f6125743ec7c60b98be
lxw-peter <1270542959@qq.com>
2022-11-16 00:54:07 +0800

    更新 tsconfig.json

--- packages/compiler-core/src/index.ts
+++ packages/compiler-core/src/index.ts
@@ -1,3 +1,3 @@
-import { sum } from '../../shared/src/a.js';
+import sum from '@mini-vue3/shared';
 let a = sum(1, 2);
-console.log(a);
+console.log('compiler-core:', a);
--- packages/reactivity/src/index.ts
+++ packages/reactivity/src/index.ts
@@ -1,3 +1,3 @@
-import { sum } from '@mini-vue3/shared';
-
-console.log('reactivity');
+import sum from '@mini-vue3/shared';
+let a = sum(3, 5);
+console.log('reactivity', a);
